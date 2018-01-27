471fb4d99974cbabacf2703d61f2142361403af6
Jolene <jwong91@gmail.com>
2018-01-26 23:42:05 -0800

    add dollys scraping

--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+## SCRAPING FACE TO FACE GAMES ##
 url = 'http://www.facetofacegames.com/products/search?query=maxx+c'
 response = requests.get(url)
 html = response.content
@@ -8,12 +10,29 @@ html = response.content
 soup = BeautifulSoup(html, 'html.parser')
 content_table = soup.find('table', attrs={'class': 'invisible-table products_table'})
 
-res = []
+f2f_res = []
 
 #traverse each table row <tr>
 for row in content_table.findAll('tr'):
-    cell_list = []
     for meta_td in row.findAll('td', attrs={'class': 'meta'}): 
+        cell_list = []
         if meta_td == '':
             continue;
-        print meta_td.get_text(";", strip=True)
+        cell_list = meta_td.get_text(';', strip=True).split(';')
+        f2f_res.append(cell_list[0:5])
+
+## SCRAPING DOLLYS ##
+url = 'http://www.dollys.ca/products/search?q=maxx+c'
+response = requests.get(url)
+html = response.content
+soup = BeautifulSoup(html, 'html.parser')
+content_list = soup.find('ul', attrs={'class': 'product-results'}) 
+
+dollys_res = []
+
+#sweet jesus unordered lists <ul> <li> <li> .....
+for li in content_list.findAll('li'):
+    print li.get_text(';', strip=True).split(';')
+
+
+
